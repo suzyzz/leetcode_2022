81580831d8c45e60411ab5ed76097b9a2d38b580
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-04-11 22:44:49 -0400

    Create 62. Unique Paths.py

---
+++ 62. Unique Paths.py
@@ -0,0 +1,26 @@
+# There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+# Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+# The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+# Method: recursive 
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 1 or n == 1:
+            return 1
+        
+        return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+      
+# Method: Dynamic Programming - Bottom-up
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        d = [[1] * n for _ in range(m)]
+
+        for col in range(1, m):
+            for row in range(1, n):
+                d[col][row] = d[col - 1][row] + d[col][row - 1]
+
+        return d[m - 1][n - 1]
+
+
