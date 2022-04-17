3f2568568d33cd383c8a4b8dbf7f5b32a6052340
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-04-16 22:31:14 -0400

    Update 15. 3Sum.py

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -64,3 +64,36 @@ class Solution:
                 else:
                     dict[nums[j]] = j
         return result
+
+#two sum       
+class Solution:
+    """
+    @param numbers: Give an array numbers of n integer
+    @return: Find all unique triplets in the array which gives the sum of zero.
+    """
+    def threeSum(self, nums):
+        nums = sorted(nums)
+        
+        results = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            self.find_two_sum(nums, i + 1, len(nums) - 1, -nums[i], results)
+            
+        return results
+
+    def find_two_sum(self, nums, left, right, target, results):
+        last_pair = None
+        while left < right:
+            if nums[left] + nums[right] == target:
+                if (nums[left], nums[right]) != last_pair:
+                    results.append([-target, nums[left], nums[right]])
+                last_pair = (nums[left], nums[right])
+                right -= 1
+                left += 1
+            elif nums[left] + nums[right] > target:
+                right -= 1
+            else:
+                left += 1   
+              
+ 
