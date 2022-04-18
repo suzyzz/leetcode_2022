26a1f29b8d6080467b2b1112bccfa3ca71b66b0d
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-04-18 16:36:41 -0400

    Create 18. 4Sum.py

---
+++ 18. 4Sum.py
@@ -0,0 +1,58 @@
+# Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+# 0 <= a, b, c, d < n
+# a, b, c, and d are distinct.
+# nums[a] + nums[b] + nums[c] + nums[d] == target
+# You may return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,0,-1,0,-2,2], target = 0
+# Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+# Example 2:
+
+# Input: nums = [2,2,2,2,2], target = 8
+# Output: [[2,2,2,2]]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 200
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+
+
+#sum(first two) = sum(last two)
+class Solution(object):
+    '''
+    题意：找到数列中所有和等于目标数的四元组，需去重
+    多枚举一个数后，参照3Sum的做法，O(N^3)    
+    '''
+    def fourSum(self, nums, target):
+        nums.sort()
+        res = []
+        length = len(nums)
+        for i in range(0, length - 3):
+            if i and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, length - 2):
+                if j != i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                sum = target - nums[i] - nums[j]
+                left, right = j + 1, length - 1
+                while left < right:
+                    if nums[left] + nums[right] == sum:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        right -= 1
+                        left += 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif nums[left] + nums[right] > sum:
+                        right -= 1
+                    else:
+                        left += 1
+        return res
