0d05de8fadd284617e7d2b504111dd6b41c7f5b4
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-04-20 18:06:56 -0400

    Create 153. Find Minimum in Rotated Sorted Array.py

---
+++ 153. Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,71 @@
+# Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+# [4,5,6,7,0,1,2] if it was rotated 4 times.
+# [0,1,2,4,5,6,7] if it was rotated 7 times.
+# Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+# Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+# You must write an algorithm that runs in O(log n) time.
+
+ 
+
+# Example 1:
+
+# Input: nums = [3,4,5,1,2]
+# Output: 1
+# Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+# Example 2:
+
+# Input: nums = [4,5,6,7,0,1,2]
+# Output: 0
+# Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+# Example 3:
+
+# Input: nums = [11,13,15,17]
+# Output: 11
+# Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+ 
+
+# Constraints:
+
+# n == nums.length
+# 1 <= n <= 5000
+# -5000 <= nums[i] <= 5000
+# All the integers of nums are unique.
+# nums is sorted and rotated between 1 and n times.
+
+#Method: 自己写的binary search
+class Solution:
+    def find_min(self, nums):
+        if not nums:
+            return 
+        
+        start = 0
+        end = len(nums) - 1
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if nums[mid] > nums[mid + 1]:
+                return nums[mid + 1]
+            elif nums[mid] > nums[end]:
+                start = mid
+            else:
+                end = mid
+        return min(nums[start], nums[end])
+      
+#Method: 关答的binary search    
+class Solution:
+    def findMin(self, nums):
+        if not nums:
+            return -1
+            
+        start, end = 0, len(nums) - 1
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if nums[mid] > nums[end]:
+                start = mid
+            else:
+                end = mid
+                
+        return min(nums[start], nums[end])
+ 
