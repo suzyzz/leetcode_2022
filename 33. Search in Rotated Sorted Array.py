4bb1efabf1051c51502bebd6856c5a2024c2e80a
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-04-20 18:43:12 -0400

    Create 33. Search in Rotated Sorted Array.py

---
+++ 33. Search in Rotated Sorted Array.py
@@ -0,0 +1,60 @@
+# There is an integer array nums sorted in ascending order (with distinct values).
+
+# Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+# Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+# Example 1:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+# Example 2:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+# Example 3:
+
+# Input: nums = [1], target = 0
+# Output: -1
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 5000
+# -104 <= nums[i] <= 104
+# All values of nums are unique.
+# nums is an ascending array that is possibly rotated.
+# -104 <= target <= 104
+
+#Method: 自己写的Binary Search
+class Solution:
+    def search(self, a, target):
+        if not a or target is None:
+            return -1
+
+        start = 0
+        end = len(a) - 1
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if a[mid] == target:
+                return mid
+            elif a[mid] >= end and a[mid] >= target and target >= a[start]:
+                end = mid
+            elif a[mid] >= end:
+                start = mid
+            elif a[mid] <= end and a[mid] <= target and target <= a[end]:
+                start = mid
+            else:
+                end = mid
+        if a[start] == target:
+            return start
+        if a[end] == target:
+            return end
+
+        return -1
+    
+
