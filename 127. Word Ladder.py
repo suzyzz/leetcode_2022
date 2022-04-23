647da62670ce9d43b169992c0e6c5aa3170c026b
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-04-22 21:30:17 -0400

    Create 127. Word Ladder.py

---
+++ 127. Word Ladder.py
@@ -0,0 +1,70 @@
+# A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+# Every adjacent pair of words differs by a single letter.
+# Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+# sk == endWord
+# Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+
+# Example 1:
+
+# Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+# Output: 5
+# Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+# Example 2:
+
+# Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+# Output: 0
+# Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+ 
+
+# Constraints:
+
+# 1 <= beginWord.length <= 10
+# endWord.length == beginWord.length
+# 1 <= wordList.length <= 5000
+# wordList[i].length == beginWord.length
+# beginWord, endWord, and wordList[i] consist of lowercase English letters.
+# beginWord != endWord
+# All the words in wordList are unique.
+
+# 使用分层遍历的BFS版本。
+
+class Solution:
+    """
+    @param: start: a string
+    @param: end: a string
+    @param: dict: a set of string
+    @return: An integer
+    """
+    def ladderLength(self, start, end, dict):
+        dict.add(end)
+        queue = collections.deque([start])
+        visited = set([start])
+        
+        distance = 0
+        while queue:
+            distance += 1
+            for i in range(len(queue)):
+                word = queue.popleft()
+                if word == end:
+                    return distance
+                
+                for next_word in self.get_next_words(word):
+                    if next_word not in dict or next_word in visited:
+                        continue
+                    queue.append(next_word)
+                    visited.add(next_word) 
+
+        return 0
+        
+    # O(26 * L^2)
+    # L is the length of word
+    def get_next_words(self, word):
+        words = []
+        for i in range(len(word)):
+            left, right = word[:i], word[i + 1:]
+            for char in 'abcdefghijklmnopqrstuvwxyz':
+                if word[i] == char:
+                    continue
+                words.append(left + char + right)
+        return words
