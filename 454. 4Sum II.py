44298c08ac3e91124433dbc0c5257f99ce19fe0d
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-04-27 21:57:04 -0400

    Create 454. 4Sum II.py

---
+++ 454. 4Sum II.py
@@ -0,0 +1,51 @@
+# Description
+# Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+
+# To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -2^28 to 2^28 - 1 and the result is guaranteed to be at most 2^31 - 1.
+
+# Example
+# Example 1:
+
+# Input:
+# A = [ 1, 2]
+# B = [-2,-1]
+# C = [-1, 2]
+# D = [ 0, 2]
+
+# Output:
+# 2
+
+# Explanation:
+# The two tuples are:
+# 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+# 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+# Example 2:
+
+# Input:
+# A = [0]
+# B = [0]
+# C = [0]
+# D = [0]
+
+# Output:
+# 1
+
+
+class Solution:
+    """
+    @param A: a list
+    @param B: a list
+    @param C: a list
+    @param D: a list
+    @return: how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero
+    """
+    def fourSumCount(self, A, B, C, D):
+        counter = {}
+        for a in A:
+            for b in B:
+                counter[a + b] = counter.get(a + b, 0) + 1
+        answer = 0
+        for c in C:
+            for d in D:
+                answer += counter.get(-c - d, 0)
+        return answer
