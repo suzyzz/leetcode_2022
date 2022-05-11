2c5e7cabeda80b53ff59a76a2fe028037b0e4027
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-05-11 18:13:32 -0400

    Update 40. Combination Sum II.py

--- 40. Combination Sum II.py
+++ 40. Combination Sum II.py
@@ -1,28 +1,28 @@
-90 · k Sum II Algorithms
-Description
-Given n distinct positive integers, the integer k (1<=k<=n1<=k<=n) and a target number.
-Find k distinct numbers within these n numbers such that the sum of these k numbers equals the target number, and you need to find all the solutions that satisfy the requirement (the order of the solutions is not required).
+# 90 · k Sum II Algorithms
+# Description
+# Given n distinct positive integers, the integer k (1<=k<=n1<=k<=n) and a target number.
+# Find k distinct numbers within these n numbers such that the sum of these k numbers equals the target number, and you need to find all the solutions that satisfy the requirement (the order of the solutions is not required).
 
-Example 1:
-Input:
-array = [1,2,3,4]
-k = 2
-target = 5
-Output:
-[[1,4],[2,3]]
-Explanation:
-1+4=5,2+3=5
+# Example 1:
+# Input:
+# array = [1,2,3,4]
+# k = 2
+# target = 5
+# Output:
+# [[1,4],[2,3]]
+# Explanation:
+# 1+4=5,2+3=5
 
-Example 2:
-Input:
-array = [1,3,4,6]
-k = 3
-target = 8
-Output:
+# Example 2:
+# Input:
+# array = [1,3,4,6]
+# k = 3
+# target = 8
+# Output:
 
-[[1,3,4]]
+# [[1,3,4]]
 
-
+#关答
 class Solution:
     """
     @param: A: an integer array
