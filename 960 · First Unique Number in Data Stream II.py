549ece930d22f7e0678ee3c5d6b9f8ce40d25be3
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-05-12 19:54:54 -0400

    Update 960 · First Unique Number in Data Stream II.py

--- 960 · First Unique Number in Data Stream II.py
+++ 960 · First Unique Number in Data Stream II.py
@@ -18,6 +18,57 @@
 # [1,2]
 # Example 2:
 
+#关答：单链表
+
+class DataStream:
+
+    def __init__(self):
+        self.dummy = ListNode(0)
+        self.tail = self.dummy
+        self.num_to_prev = {}
+        self.duplicates = set()
+          
+    """
+    @param num: next number in stream
+    @return: nothing
+    """
+    def add(self, num):
+        if num in self.duplicates:
+            return
+        
+        if num not in self.num_to_prev:
+            self.push_back(num)            
+            return
+        
+        # find duplicate, remove it from hash & linked list
+        self.duplicates.add(num)
+        self.remove(num)
+    
+    def remove(self, num):
+        prev = self.num_to_prev.get(num)
+        del self.num_to_prev[num]
+        prev.next = prev.next.next
+        if prev.next:
+            self.num_to_prev[prev.next.val] = prev
+        else:
+            # if we removed the tail node, prev will be the new tail
+            self.tail = prev
+
+    def push_back(self, num):
+        # new num add to the tail
+        self.tail.next = ListNode(num)
+        self.num_to_prev[num] = self.tail
+        self.tail = self.tail.next
+
+    """
+    @return: the first unique number in stream
+    """
+    def firstUnique(self):
+        if not self.dummy.next:
+            return None
+        return self.dummy.next.val
+    
+    
 #关答：双链表 O(1), O(1)
 class Node:
     def __init__(self, value=None, prev=None, next=None):
