a197debf7b5df88400a07973d5e984beef848d21
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-05-17 15:04:41 -0400

    Update 120. Triangle.py

--- 120. Triangle.py
+++ 120. Triangle.py
@@ -66,3 +66,41 @@ class Solution:
         right = self.dfs_divide_conquer(triangle, x + 1, y + 1, memo)
         memo[(x, y)] = min(left, right) + triangle[x][y]
         return memo[(x, y)]
+
+#Method 4: DP-for-loop-Top-down
+class Solution:
+    def minimum_total(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)        
+        dp = [[0] * (i + 1) for i in range(n)]
+        
+        dp[0][0] = triangle[0][0]
+        for i in range(1, n):
+            dp[i][0] = dp[i - 1][0] + triangle[i][0]
+            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i]
+
+        for i in range(2, n):
+            for j in range(1, i):
+                dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j]
+            
+        return min(dp[n - 1])
+
+#Method 5: DP-for-loop-Bottom-Up
+class Solution:
+    def minimum_total(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)        
+        dp = [[0] * (i + 1) for i in range(n)]
+
+        for i in range(n):
+            dp[n - 1][i] = triangle[n - 1][i]
+
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1):
+                dp[i][j] = min(dp[i + 1][j], dp[i + 1][j +1]) + triangle[i][j]
+            
+        return dp[0][0]
+
+
+
+
+
+
