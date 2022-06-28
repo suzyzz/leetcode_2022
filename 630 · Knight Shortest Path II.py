9dcc823d2cd6febbf7b26b061c3fb145fcfb93f9
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-06-28 13:03:57 -0400

    Update 630 · Knight Shortest Path II.py

--- 630 · Knight Shortest Path II.py
+++ 630 · Knight Shortest Path II.py
@@ -127,3 +127,36 @@ class Solution:
         if dp[n - 1][m - 1] == float('inf'):
             return -1
         return dp[n - 1][m - 1]
+
+    
+#   Method 3 - DP - Rolling Array
+
+DIRECTIONS = [(-1, -2), (1, -2), (-2, -1), (2, -1)]
+class Solution:
+    def shortestPath2(self, grid):
+        if not grid or not grid[0]:
+            return -1
+        
+        n, m = len(grid), len(grid[0])
+        
+        # state: dp[i][j % 3] 代表从 0,0 跳到 i,j 的最少步数
+        dp = [[float('inf')] * 3 for _ in range(n)]
+
+        # initialize: 0,0 是起点
+        dp[0][0] = 0
+        
+        # function
+        for j in range(1, m):
+            for i in range(n):
+                dp[i][j % 3] = float('inf')
+                if grid[i][j]:
+                    continue
+                for delta_x, delta_y in DIRECTIONS:
+                    x, y = i + delta_x, j + delta_y
+                    if 0 <= x < n and 0 <= y < m:
+                        dp[i][j % 3] = min(dp[i][j % 3], dp[x][y % 3] + 1)
+
+        # answer
+        if dp[n - 1][(m - 1) % 3] == float('inf'):
+            return -1
+        return dp[n - 1][(m - 1) % 3]
