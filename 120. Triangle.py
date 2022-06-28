f599c15757fe34792ad25b0253c973f7149701f2
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-06-28 12:42:40 -0400

    Update 120. Triangle.py

--- 120. Triangle.py
+++ 120. Triangle.py
@@ -99,8 +99,21 @@ class Solution:
             
         return dp[0][0]
 
+#Method 6: DP-for-loop-Top-down, rolling array
+class Solution:
+    def minimum_total(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)        
+        dp = [[0] * n, [0] * n]
+        
+        dp[0][0] = triangle[0][0]
+        for i in range(1, n):
+            dp[i % 2][0] = dp[(i - 1) % 2][0] + triangle[i][0]
+            dp[i % 2][i] = dp[(i - 1) % 2][i - 1] + triangle[i][i]
 
-
+            for j in range(1, i):
+                dp[i % 2][j] = min(dp[(i - 1) % 2][j - 1], dp[(i - 1) % 2][j]) + triangle[i][j]
+            
+        return min(dp[(n - 1) % 2])
 
 
 
