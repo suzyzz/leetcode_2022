bedd930c44fd6f7f33b9cfe0d2e1e569165484ee
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-09-18 09:53:16 -0400

    Update 610 · Two Sum - Difference equals to target.py

--- 610 · Two Sum - Difference equals to target.py
+++ 610 · Two Sum - Difference equals to target.py
@@ -50,3 +50,33 @@ class Solution:
             if nums[j] - nums[i] == target: # 找到答案
                 return [nums[i],nums[j]]
         return [-1,-1]
+
+#     用二分法替代hash - O(nlogn) - 不是最优解但也是关答
+class Solution:
+    """
+    @param nums: an array of Integer
+    @param target: an integer
+    @return: [num1, num2] (index1 < index2)
+    """
+    def two_sum7(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1, -1]
+        target = abs(target)
+        for i in range(len(nums)):
+            j = self.binary_search(nums, i + 1, len(nums) - 1, target + nums[i])
+            if j != -1:
+                return [nums[i], nums[j]]
+        return [-1, -1]
+    
+    def binary_search(self, nums, start, end, target):
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if nums[mid] < target:
+                start = mid
+            else:
+                end = mid
+        if nums[start] == target:
+            return start
+        if nums[end] == target:
+            return end
+        return -1
