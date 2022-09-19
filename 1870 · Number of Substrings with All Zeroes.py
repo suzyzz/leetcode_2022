d7fb3081c66f6ef7fb0db5b1dd71b38c54244074
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-09-19 12:50:23 -0400

    Create 1870 · Number of Substrings with All Zeroes.py

---
+++ 1870 · Number of Substrings with All Zeroes.py
@@ -0,0 +1,42 @@
+# Description
+# Given a string str containing only 0 or 1, please return the number of substrings that consist of 0 .
+
+# 1<=|str|<=30000
+
+# Example
+# Example 1:
+
+# Input:
+# "00010011"
+# Output:
+# 9
+# Explanation:
+# There are 5 substrings of "0",
+# There are 3 substrings of "00",
+# There is 1 substring of "000".
+# So return 9
+# Example 2:
+
+# Input:
+# "010010"
+# Output:
+# 5
+
+# 关答-同向双指针-O(n)
+
+class Solution:
+    """
+    @param str: the string
+    @return: the number of substrings 
+    """
+    def string_count(self, str: str) -> int:
+        count, answer = 0, 0
+        for i in str:
+            if i == "0":
+                count += 1
+            else:
+                answer += (count + 1) * count // 2
+                count = 0
+        if count != 0:
+            answer += (count + 1) * count // 2
+        return answer
