53a5679a259897fc7391b6b8fd06683986e87262
suzyzz <63985611+suzyzz@users.noreply.github.com>
2022-10-02 22:59:09 -0400

    Update 600 · Smallest Rectangle Enclosing Black Pixels.py

--- 600 · Smallest Rectangle Enclosing Black Pixels.py
+++ 600 · Smallest Rectangle Enclosing Black Pixels.py
@@ -16,74 +16,92 @@
 # The upper left coordinate of the matrix is (0, 0), and the lower right coordinate is (1,2).
 
 # 关答-两次二分 - O(MlogN+NlogM)
-
-class Solution(object):
-    # @param image {List[List[str]]}  a binary matrix with '0' and '1'
-    # @param x, y {int} the location of one of the black pixels
-    # @return an integer
+class Solution:
+    """
+    @param image: a binary matrix with '0' and '1'
+    @param x: the location of one of the black pixels
+    @param y: the location of one of the black pixels
+    @return: an integer
+    """
     def minArea(self, image, x, y):
-        # Write your code here
-        m = len(image)
-        if m == 0:
-            return 0
-        n = len(image[0])
-        if n == 0:
+        if not image or not image[0]:
             return 0
 
-        start = y
-        end = n - 1
-        while start < end:
-            mid = start + (end - start) / 2 + 1
-            if self.checkColumn(image, mid):
-                start = mid
-            else:
-                end = mid - 1
+        n, m = len(image), len(image[0])
+        up = self.binarySearch(image, 0, x, self.getRowColor, '1')
+        down = self.binarySearch(image, x, n - 1, self.getRowColor, '0')
+        left = self.binarySearch(image, 0, y, self.getColumnColor, '1')
+        right = self.binarySearch(image, y, m - 1, self.getColumnColor, '0')
 
-        right = start
+        return (down - up) * (right - left)
 
-        start = 0
-        end = y
-        while start < end:
-            mid = start + (end - start) / 2
-            if self.checkColumn(image, mid):
+    def binarySearch(self, image, start, end, getColorFunc, color):
+        # 在 start 到 end 这段区间内
+        # 找到第一个 index 使得 getColorFunc(index) == color
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if getColorFunc(image, mid) == color:
                 end = mid
             else:
-                start = mid + 1
-
-        left = start
-        
-        start = x
-        end = m - 1
-        while start < end:
-            mid = start + (end - start) / 2 + 1
-            if self.checkRow(image, mid):
                 start = mid
-            else:
-                end = mid - 1
+        if getColorFunc(image, start) == color:
+            return start
+        if getColorFunc(image, end) == color:
+            return end
+        return end + 1
 
-        down = start
-        
-        start = 0
-        end = x
-        while start < end:
-            mid = start + (end - start) / 2
-            if self.checkRow(image, mid):
+    def getRowColor(self, image, rowIndex):
+        for i in range(len(image[0])):
+            if image[rowIndex][i] == '1':
+                return '1'
+        return '0'
+
+    def getColumnColor(self, image, colIndex):
+        for i in range(len(image)):
+            if image[i][colIndex] == '1':
+                return '1'
+        return '0'class Solution:
+    """
+    @param image: a binary matrix with '0' and '1'
+    @param x: the location of one of the black pixels
+    @param y: the location of one of the black pixels
+    @return: an integer
+    """
+    def minArea(self, image, x, y):
+        if not image or not image[0]:
+            return 0
+
+        n, m = len(image), len(image[0])
+        up = self.binarySearch(image, 0, x, self.getRowColor, '1')
+        down = self.binarySearch(image, x, n - 1, self.getRowColor, '0')
+        left = self.binarySearch(image, 0, y, self.getColumnColor, '1')
+        right = self.binarySearch(image, y, m - 1, self.getColumnColor, '0')
+
+        return (down - up) * (right - left)
+
+    def binarySearch(self, image, start, end, getColorFunc, color):
+        # 在 start 到 end 这段区间内
+        # 找到第一个 index 使得 getColorFunc(index) == color
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if getColorFunc(image, mid) == color:
                 end = mid
             else:
-                start = mid + 1
+                start = mid
+        if getColorFunc(image, start) == color:
+            return start
+        if getColorFunc(image, end) == color:
+            return end
+        return end + 1
 
-        up = start
-        
-        return (right - left + 1) * (down - up + 1)
+    def getRowColor(self, image, rowIndex):
+        for i in range(len(image[0])):
+            if image[rowIndex][i] == '1':
+                return '1'
+        return '0'
 
-    def checkColumn(self, image, col):
-        for i in xrange(len(image)):
-            if image[i][col] == '1':
-                return True
-        return False
-
-    def checkRow(self, image, row):
-        for j in xrange(len(image[0])):
-            if image[row][j] == '1':
-                return True
-        return False
+    def getColumnColor(self, image, colIndex):
+        for i in range(len(image)):
+            if image[i][colIndex] == '1':
+                return '1'
+        return '0'
